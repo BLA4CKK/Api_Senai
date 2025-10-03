59bd84dc6b2426921122274739f7baec21cce4e1
BLA4CKK <lopesmachadopedrohenrique@gmail.com>
2025-10-03 15:57:35 -0300

    03/10

---
+++ front/Index/script.js
@@ -0,0 +1,7 @@
+const email = document.querySelector('#email')
+const senha = document.querySelector('#senha')
+const cadastro = document.querySelector('#cadastrar')
+
+cadastro.addEventListener('click', ()=>{
+    
+})
